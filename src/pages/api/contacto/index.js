f187b6955f8e5fd98890c14177a5a5c590be1e4c
tomcat7733 <tomcat7733@gmail.com>
2025-04-16 18:32:43 +0200

    Update index.js

--- src/pages/api/contacto/index.js
+++ src/pages/api/contacto/index.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { nombre, apellidos, email, titulo, mensaje, acepta_terminos } = req.body;
 
-    if (!nombre || apellidos || !email || !titulo || !mensaje !== true) {
+    if (!nombre || !apellidos || !email || !titulo || !mensaje !== true) {
       return res.status(400).json({ message: 'Faltan campos obligatorios o términos no aceptados' });
     }
 
