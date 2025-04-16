eeb292806eb94b8b9bfd8a82cec9f5cd3c0ca540
tomcat7733 <tomcat7733@gmail.com>
2025-04-16 20:02:13 +0200

    Update index.js

--- src/pages/api/libros/index.js
+++ src/pages/api/libros/index.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
         usuarios:usuario_id (
           nombre_usuario
         ),
-        generos!libros_genero_id_fkey (
+        generos (
           nombre
         )
       `);
