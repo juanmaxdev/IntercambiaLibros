a4df887c2d006aa51ff8624c7657089557c8fb56
tomcat7733 <tomcat7733@gmail.com>
2025-04-17 10:59:18 +0200

    Update index.js

--- src/pages/api/valoraciones/index.js
+++ src/pages/api/valoraciones/index.js
@@ -30,7 +30,6 @@ export default async function handler(req, res) {
         usuario_id,
         valoracion,
         comentario,
-        fecha_valoracion,
         titulo
       }
     ]).select();
