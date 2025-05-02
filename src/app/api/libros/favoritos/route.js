5d9e2e636e47c101910c4161987910ef8363d33f
tomcat7733 <tomcat7733@gmail.com>
2025-05-02 21:50:38 +0200

    Favoritos y delete
    Se implemente la opcion de favoritos, las opciones de delete.
    
    Se habilitan las diferentes pestañas en "mis libros" y tambien se ultiman los botones de los libros en el menú del usuario.

---
+++ src/app/api/libros/favoritos/route.js
@@ -0,0 +1,34 @@
+import { librosFavoritos, agregarLibroAFavoritos, eliminarLibroDeFavoritos, } from "@/services/favoritosService";
+  
+  export async function GET() {
+    try {
+      const libros = await librosFavoritos();
+      return Response.json(libros);
+    } catch (err) {
+      console.error("Error en la API de favoritos:", err);
+      return Response.json({ error: err.message || "Error interno" }, { status: 500 });
+    }
+  }
+  
+  export async function POST(request) {
+    try {
+      const { id_libro } = await request.json();
+      await agregarLibroAFavoritos(id_libro);
+      return Response.json({ message: "Libro añadido a favoritos" });
+    } catch (err) {
+      console.error("Error en la API POST favoritos:", err);
+      return Response.json({ error: err.message || "Error interno" }, { status: 500 });
+    }
+  }
+  
+  export async function DELETE(request) {
+    try {
+      const { id_libro } = await request.json();
+      await eliminarLibroDeFavoritos(id_libro);
+      return Response.json({ message: "Libro eliminado de favoritos" });
+    } catch (err) {
+      console.error("Error en la API DELETE favoritos:", err);
+      return Response.json({ error: err.message || "Error interno" }, { status: 500 });
+    }
+  }
+  
