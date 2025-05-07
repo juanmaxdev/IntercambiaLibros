1928a9e95ef81dbaa782f030d38447aa99a39e6e
tomcat7733 <tomcat7733@gmail.com>
2025-05-07 16:32:19 +0200

    ruta serversesion arreglado

--- src/app/api/mensajes/route.js
+++ src/app/api/mensajes/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 import { supabase } from "@/lib/supabase"
-import { getServerSession } from "next-auth"
+import { getServerSession } from "@/server/auth"
 
 // Obtener mensajes
 export async function GET(request) {
